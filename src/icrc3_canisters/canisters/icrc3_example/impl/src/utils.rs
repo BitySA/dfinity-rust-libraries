6a57fbb3b16ab5e6e230fa1beb029f287331b63e
Gautier Wojda <gautier.wojda@bity.com>
2025-06-17 09:47:03 +0200

    fix compil

--- src/icrc3_canisters/canisters/icrc3_example/impl/src/utils.rs
+++ src/icrc3_canisters/canisters/icrc3_example/impl/src/utils.rs
@@ -1,6 +1,6 @@
 pub fn trace(msg: &str) {
     unsafe {
-        ic0::debug_print(msg.as_ptr() as i32, msg.len() as i32);
+        ic0::debug_print(msg.as_ptr() as usize, msg.len() as usize);
     }
     ic_cdk::println!("{}", msg);
 }
